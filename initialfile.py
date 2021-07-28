66ecb39ac913a5a5ebbf20e5ca453b4fc14dc42c
SamyagJ <53125663+SamyagJ@users.noreply.github.com>
2021-07-27 19:20:06 -0700

    Update initialfile.py

--- initialfile.py
+++ initialfile.py
@@ -1,1 +1,17 @@
+#import time
+
+print("This is an intial file")
+
+l = [1,2,3,4,5]
+
+def list_output(l):
+    n=0
+    for i in l:
+        if i>2:
+            print("Greater than the first prime number")
+            n=n+1
+
+    return ("There are %s numbers in this list which are greater than 2", n)
+
+#This is a test file
 
